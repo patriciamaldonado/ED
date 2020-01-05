fc685da8532692c87dc930751a01d123240746c7
Patricia <patriimaldonado@gmail.com>
2020-01-05 03:09:34 +0100

    añado cifras comienzo

---
+++ cifras/cifras.cpp
@@ -0,0 +1,132 @@
+
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include "cifras.h"
+
+using namespace std;
+
+static int cont=0;
+
+bool restaPosible(int a, int b){
+  bool posible=true;
+  if(a<=b)
+    posible=false;
+  return posible;
+}
+
+bool divisionPosible(int a, int b){
+  bool posible=false;
+  if(a>b)
+    if(a%b==0)    posible=true;
+  return posible;
+}
+//En el vector v busca los enteros a y b y los borra
+void borrar(vector<int> &v,int a, int b){
+  int i=0, tam=v.size();
+  bool encA=false, encB=false;
+  while(i<tam){
+    if(v[i]==a and !encA){
+      v.erase(v.begin()+i);
+      encA=true;
+      tam--;
+    }
+    else if(v[i]==b and !encB){
+      v.erase(v.begin()+i);
+      encB=true;
+      tam--;
+    }
+    else
+      i++;
+  }
+}
+//Esta el entero result en el vector v?
+bool esta(vector<int> v, int result){
+  bool esta=false;
+  for(int i=0; i<v.size() and !esta; i++)
+    if(v[i]==result)   esta=true;
+  return esta;
+}
+
+void mostrar(solucion v){
+  for(int i=0; i<v.operaciones.size(); i++)
+    cout << v.operaciones[i][0] << v.operando[i]<< v.operaciones[i][1] << " = "<< v.operaciones[i][2] << endl;
+  cout << endl;
+}
+//Funcion para evitar la repetición de código en la funcion cyl
+solucion codigo(solucion sol, char oper, int i, int j){
+  solucion s=sol;
+  int operacion;
+  if(oper == '+')   operacion=s.elementos[i]+s.elementos[j];
+  if(oper == '-')   operacion=s.elementos[i]-s.elementos[j];
+  if(oper == 'x')   operacion=s.elementos[i]*s.elementos[j];
+  if(oper == '/')   operacion=s.elementos[i]/s.elementos[j];
+  vector<int> op;
+  op.push_back(s.elementos[i]);
+  op.push_back(s.elementos[j]);
+  op.push_back(operacion);
+  s.operaciones.push_back(op);
+  s.operando.push_back(oper);
+  borrar(s.elementos, s.elementos[i], s.elementos[j]);
+  s.elementos.push_back(operacion);
+  return s;
+}
+
+void cyl(solucion sol, int resultado, int tam, vector<int> el){
+  if(tam==1 ){
+    if(sol.elementos[0]==resultado){
+      mostrar(sol);
+      cont++;
+    }
+  }
+  else if(esta(sol.elementos, resultado)){
+    mostrar(sol);
+    cont++;
+  }
+  else{
+    int operacion,i,j;
+    solucion s;
+    for(i=0; i<tam; i++){
+      for(j=0; j<tam;j++){
+        if(i<j){
+          s=codigo(sol,'+',i,j); //SUMA
+          cyl(s,resultado,tam-1,el);
+          s=codigo(sol,'x',i,j); //MULTIPLICACION
+          cyl(s,resultado,tam-1,el);
+        }
+        if(i!=j){
+          if(restaPosible(sol.elementos[i], sol.elementos[j])){
+            s=codigo(sol,'-',i,j);  //RESTA
+            cyl(s,resultado,tam-1,el);
+          }
+          if(divisionPosible(sol.elementos[i], sol.elementos[j])){
+            s=codigo(sol,'/',i,j);  //DIVISION
+            cyl(s,resultado,tam-1,el);
+          }
+        }
+      }
+    }
+  }
+}
+
+int main(){
+  solucion v;
+  int num=-1, resultado;
+
+  while(num!=0){
+    system("clear");
+    cout << "NUMEROS A OPERAR" << endl;
+    for(int i=0; i<v.elementos.size(); i++)
+      cout << "| " << v.elementos[i] << " ";
+    cout << "|" << endl;
+    cout << "Introduce un número y pulsa enter (para dejar de meter numeros introduce 0)" << endl;
+    cin >> num;
+    if(num!=0)
+      v.elementos.push_back(num);
+  }
+
+  cout << "Ahora introduce el resultado a conseguir" << endl;
+  cin >> resultado;
+  cyl(v,resultado,v.elementos.size(),v.elementos);
+  cout << "SOLUCIONES ENCONTRADAS: " << cont << endl;
+}
---
+++ cifras/cifras.h
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+
+class solucion{
+private:
+  vector<int> elementos;    //elementos disponibles para realizar operaciones
+  vector<vector<int> > operaciones; //tripletas correspondientes a las operaciones hasta llegar a la solucion
+  vector<char> operando;    //vector de los operadores en orden
+public:
+bool restaPosible(int a, int b);
+bool divisionPosible(int a, int b);
+void borrar(vector<int> &v,int a, int b);
+bool esta(vector<int> v, int result);
+void mostrar(solucion v);
+solucion codigo(solucion sol, char oper, int i, int j); // ?????????????????
+void cyl(solucion sol, int resultado, int tam, vector<int> el);
+
+};
